54114328625aa1c609aeca28dca1b0c11f284392
Chrissie Aldo <chriz.scaarlet.27@gmail.com>
2022-10-03 10:03:00 +0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,38 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+ 
+   i<-NULL
+  set<-function(y){
+    
+    x<<-y
+    i<<-NULL
+    
+  }
+  
+  get<-function() x
+  
+  set_inv <- function(inv) i<<-inv
+  get_inv <- function() i
+  
+  list(set=set,get=get,set_inv = set_inv, get_inv = get_inv)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+       
+   i<- x$get_inv()
+  if(!is.null(i)){
+    
+    message("Getting Cached Data...")
+    return(i)
+    
+  }
+  
+   data<-x$get()
+   i<-solve(data, ...)
+   x$set_inv(i)
+   i
 }
